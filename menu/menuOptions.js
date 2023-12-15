7936268067f207f7800405a82df5b0b0371ee8f7
matheu <matheusviaira160@gmail.com>
2023-12-15 18:01:08 -0300

    refactor: 💡 modularization
    arquitetura de arquivos que armazena respostas das perguntas

--- menu/menuOptions.js
+++ menu/menuOptions.js
@@ -1,11 +1,12 @@
 const createMenu = (text, options = [], questions = []) => ({ text, options, questions });
 // Exemplo de estrutura aninhada
-const menuOptions = createMenu('Escolha um número para explorar nossos serviços:', [
-  createMenu('Seguros', [
+const menuOptions = createMenu('Para continuarmos, por favor, selecione a opção que melhor atende às suas necessidades:', [
+  createMenu('1️⃣ Seguro auto 🚗', [
     createMenu('Seguro Auto', [
       createMenu('Doação', [], [
         { text: 'Qual é o seu nome?', answer: '' },
         { text: 'Qual é o seu CPF?', answer: '' },
+        { text: 'Uma letra, qualquer letra?', answer: '' },
         // Adicione mais perguntas conforme necessário
       ]),
       createMenu('Opção1', [], []),
@@ -21,15 +22,53 @@ const menuOptions = createMenu('Escolha um número para explorar nossos serviço
     createMenu('Previdência', [], []),
     createMenu('Consórcio', [], []),
   ]),
-  createMenu('Compra', [
+  createMenu('2️⃣ Seguro moto 🛵', [
+    createMenu('Compra Auto', [], [
+      { text: 'Qual é o seu nome?', answer: '' },
+      { text: 'Qual é o seu CPF?', answer: '' },
+    ]),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu('3️⃣ Plano de saúde 🩺', [
     createMenu('Compra Auto', [], []),
     createMenu('Compra Casa', [], []),
     createMenu('Compra Outros', [], []),
   ]),
-  createMenu('Venda', [
+  createMenu('4️⃣ Plano dental 🦷', [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu('5️⃣ Plano Pet 🐶 🐱', [
     createMenu('Venda Auto', [], []),
     createMenu('Venda Casa', [], []),
     createMenu('Venda Outros', [], []),
+  ]),
+  createMenu("6️⃣ Telemedicina 24:00 hrs 📱", [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu("7️⃣ Seguro de Vida 👨‍👨‍👧‍👦", [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu("8️⃣ Previdência Privada 💰", [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu("9️⃣ Consórcio 🏠🚙🚛🏍🚴", [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
+  ]),
+  createMenu("🔟 Atendimento humano", [
+    createMenu('Compra Auto', [], []),
+    createMenu('Compra Casa', [], []),
+    createMenu('Compra Outros', [], []),
   ]),
 ]);
 
